03b7e0e303755371c2911b1deb5a39b6ee52fbdd
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-09 10:13:57 +0300

    Initialized the project

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,5 +1,5 @@
-import Header from '@/components/layouts/Header';
-import Footer from '@/components/layouts/Footer';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
